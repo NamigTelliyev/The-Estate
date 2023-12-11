88ae4c4557f3159c7f7f24a5e5a32f68c4430884
NamigTelliyev <146194992+NamigTelliyev@users.noreply.github.com>
2023-12-11 17:38:44 +0400

    the estate

---
+++ assets/js/home.js
@@ -0,0 +1,8 @@
+const product=document.getElementById("product");
+const btn=document.getElementById("btn");
+
+function getProduct(){
+let card=JSON.parse(localStorage.getItem("card")) || []
+products.innerHTML = ''
+card.item()
+}
